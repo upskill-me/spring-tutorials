d9010ea4e38d92b9f73762c137b7a3969701472f
Ritesh Sangwan <sangwan.ritesh@yahoo.in>
2023-01-02 12:44:05 +0530

    keep directory structure

---
+++ school-management/src/main/java/me/upskill/springtutorials/sklmgmt/Main.java
@@ -0,0 +1,16 @@
+package me.upskill.springtutorials.sklmgmt;
+
+/**
+ * A dummy main class
+ */
+public class Main {
+
+    /**
+     * Main method invoked by jvm
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
